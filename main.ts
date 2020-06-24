6afb376b7bcdbe723443b2327ec76be4948cb067
MINT Genie <66748747+MINTGenie@users.noreply.github.com>
2020-06-24 23:13:20 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,13 @@
+radio.onReceivedValue(function (name, value) {
+    if (name == "x") {
+        robotbit.Servo(robotbit.Servos.S1, value)
+        robotbit.Servo(robotbit.Servos.S3, value)
+    } else if (name == "y") {
+        robotbit.Servo(robotbit.Servos.S2, value)
+        robotbit.Servo(robotbit.Servos.S4, value)
+    }
+})
+radio.setGroup(15)
+basic.forever(function () {
+	
+})
